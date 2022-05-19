fd0eccb181dfe8f415d2dc3b058bb52a9acc6de8
ddsardina <sardinadaniel8@gmail.com>
2022-05-19 09:26:36 -0700

    Add the ability to connect to a PSQL DB

--- myproject/__init__.py
+++ myproject/__init__.py
@@ -3,12 +3,19 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
+#cfrom myproject.config import dbpass
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'mysecretkey'
+###Configuration for sqlite DB
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
+
+###Configuration for Postgresql DB
+#Create a Database called brawlstars, User: application, password stored in dbpass variable in config.py
+#app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://application:{dbpass}@localhost:5432/brawlstars'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
